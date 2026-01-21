96cad3a1d2de41966ad9e3c741de25f1be1a8131
Isaias Silva <isaiasilva.info@gmail.com>
2026-01-21 12:31:07 -0300

    fix error build

--- src/pages/Carteira.tsx
+++ src/pages/Carteira.tsx
@@ -2566,30 +2566,7 @@ const WalletDashboard = () => {
     </div>
   );
 };
-<svg className="w-5 h-5 text-red-600" fill="currentColor" viewBox="0 0 20 20">
-  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
-</svg>
-          </div >
-          <div className="flex-1">
-            <h3 className="text-sm font-medium text-red-800">Erro ao criar subconta</h3>
-            <p className="mt-1 text-sm text-red-700">{subAccountError}</p>
-          </div>
-          <button
-            onClick={() => setSubAccountError(null)}
-            className="flex-shrink-0 text-red-400 hover:text-red-600"
-          >
-            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-              <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
-            </svg>
-          </button>
-        </div >
-      </motion.div >
-    </div >
-  )
-}
-    </div >
-  );
-};
+
 
 
 export default WalletDashboard;
